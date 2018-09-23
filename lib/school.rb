3d010082d9291f17082b2cc4424299083a2cf506
Indira Kissoondyal <indirak@alumni.princeton.edu>
2018-09-23 20:31:24 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -10,7 +10,7 @@ class School
 
   def add_student(student_name, grade)
     
-    if roster[grade]
+    if @roster[grade]
       @roster[grade] << student_name
     else 
       @roster[grade] = []
