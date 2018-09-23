757b15c2a126c3aafd3eb618809f98a5dd66ad4f
Indira Kissoondyal <indirak@alumni.princeton.edu>
2018-09-23 20:32:48 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -4,13 +4,18 @@ class School
   attr_accessor :school_name, :roster, :student_name :grade
 
   def initialize(school_name)
+    @school_name = school_name 
     @roster = {}
   end 
 
   def add_student(student_name, grade)
     
-    roster[grade] = []
-    roster[grade] << student_name
+    if @roster[grade]
+      @roster[grade] << student_name
+    else 
+      @roster[grade] = []
+      @roster[grade] << student_name
+    end 
   end 
 
 end 
